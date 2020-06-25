c6507fb21b2bb17b2650d0e8066b77f68a6df51b
plammel <lammelpablo@gmail.com>
2020-06-25 10:56:33 -0300

    feat(GESTOR): Remueve permiso Mis Solicitudes

--- auth/permisos.ts
+++ auth/permisos.ts
@@ -352,11 +352,6 @@ export default [
                 type: 'boolean'
             },
             {
-                key: 'asignadas',
-                title: 'Mis Solicitudes',
-                type: 'boolean'
-            },
-            {
                 key: 'anular',
                 title: 'Anular Solicitudes',
                 type: 'boolean'
